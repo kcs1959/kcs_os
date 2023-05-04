f31b490d649a6cff71e733cdf5fa60e8499547bd
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2023-05-04 17:28:49 +0900

    Impl Hello world UEFI Application
    Co-authored-by: yasufumi <yasufumi-nakata@users.noreply.github.com>
    Co-authored-by: ato lash <hal-shu-sato@users.noreply.github.com>

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![no_std]
+#![no_main]
+
+use uefi::prelude::*;
+use uefi_services::println;
+
+#[entry]
+fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    uefi_services::init(&mut system_table).unwrap();
+    println!("Hello world from uefi-rs!");
+    println!("Sanjo");
+    println!("unkonow");
+    println!("Aiueo");
+    loop {}
+    Status::SUCCESS
+}
