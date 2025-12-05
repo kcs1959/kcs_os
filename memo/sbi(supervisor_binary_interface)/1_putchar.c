a2627c8a743bdcc0039c90ab561ef6dbf5540c83
Latte72R <ryofuji2005@gmail.com>
2025-12-05 22:53:42 +0900

    Remove memo directory

--- memo/sbi(supervisor_binary_interface)/1_putchar.c
+++
@@ -1,7 +0,0 @@
-#include "../../common.h"
-#include "./sbi_call_test.c"
-
-void putchar(char ch) { sbi_call(ch, 0, 0, 0, 0, 0, 0, 1); }
-
-// 以下は、ユーザーアプリケーションでの使用例
-void main(void) { printf("Hello World from shell!\n"); }
